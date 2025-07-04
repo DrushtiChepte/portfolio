6325713e4ac58f9e0bb0cd86845f6e8eefeaa4b6
Drushti Chepte <cheptedrushti@gmail.com>
2025-07-04 14:03:08 +0530

    fix spell error

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -35,7 +35,7 @@ const Hero = () => {
             From Mechanical roots to Modern Web routes.
           </p>
           <div className="flex justify-center items-center space-x-6 pt-10">
-            <a href="/drushtiChepte_FDResume_2025.pdf" download>
+            <a href="/DrushtiChepte_FDResume_2025.pdf" download>
               <button className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none">
                 <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                 <span className="inline-flex h-full w-full items-center justify-center gap-2 rounded-lg bg-slate-950 px-5 py-1 text-sm font-medium text-white backdrop-blur-3xl">
