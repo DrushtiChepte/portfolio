824820fefe55be2325ed13ead58f8af0e2435ef0
Drushti Chepte <cheptedrushti@gmail.com>
2025-07-09 23:35:44 +0530

    hero btn fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -48,7 +48,7 @@ const Hero = () => {
                 download
                 aria-label="Download Drushti Chepte resume"
               >
-                <button className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none">
+                <button className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none cursor-pointer">
                   <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                   <span className="inline-flex h-full w-full items-center justify-center gap-2 rounded-lg bg-slate-950 px-5 py-1 text-sm font-medium text-white backdrop-blur-3xl">
                     Download Resume
